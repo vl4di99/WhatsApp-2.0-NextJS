6c5dbbd019e1a8c1351c5b00582d0940ea4b54c1
Dionisie-Vladut Lorincz <dionisie.lorincz@yahoo.com>
2022-07-28 17:23:25 +0300

    Initial commit. Added styled-components and material-ui

---
+++ components/Sidebar.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+function Sidebar() {
+  return (
+    <Container>
+      <Header></Header>
+    </Container>
+  );
+}
+
+export default Sidebar;
+
+const Container = styled.div``;
+
+const Header = styled.div``;
